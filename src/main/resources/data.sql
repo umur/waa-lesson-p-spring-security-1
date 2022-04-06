199befa5bd17789a4e19db9654b6d1f830f5cb65
Muhyieddin <muhyidean@gmail.com>
2022-04-06 03:06:17 -0500

    Muhyieddin initial

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,11 +2,15 @@ INSERT INTO users (id, email, firstname, lastname, password)
 VALUES (1, 'uinan@miu.edu', 'umur', 'inan', '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0pS7XLCHO4yHZ.BW/yvWu1feo2'); --123
 INSERT INTO users (id, email, firstname, lastname, password)
 VALUES (2, 'john@miu.edu', 'john', 'doe', '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0pS7XLCHO4yHZ.BW/yvWu1feo2'); --123
+INSERT INTO users (id, email, firstname, lastname, password)
+VALUES (3, 'dean@miu.edu', 'Dean', 'Altarawneh', '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0pS7XLCHO4yHZ.BW/yvWu1feo2'); --123
 
 INSERT INTO role (id, role)
 VALUES (1, 'ADMIN');
 INSERT INTO role (id, role)
 VALUES (2, 'CLIENT');
+INSERT INTO role (id, role)
+VALUES (3, 'CLIENT');
 
 INSERT INTO users_roles (user_id, roles_id)
 VALUES (1, 1);
